25b36235db0ebc72ce1512f095ae411ba45ac7be
kamiinna2305 <130962378+kamiinna2305@users.noreply.github.com>
2025-02-20 15:58:15 +0100

    Add files via upload

---
+++ lesson_5_task_1/src/components/ListProducts/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+<product-item
+v-for="product in listProductsData"
+:key="product.id"
+:product-data="product"
+/>
+    </div>
+</template>
+
+<script>
+import ProductItem from '@/components/ListProducts/ProductItem.vue'
+    export default {
+        name: 'ListProducts',
+        components: {
+            ProductItem,
+        },
+      props: {
+        listProductsData: {
+            type: Array,
+            default: () => []
+        },
+      },
+        
+    }
+</script>
+
+<style  scoped>
+.container{
+    display: flex;
+    gap: 10px;
+    padding: 25px;
+}
+</style>
