40eb83dd408b59a52ef7d6d7e08a572a04143853
kamiinna2305 <130962378+kamiinna2305@users.noreply.github.com>
2025-02-20 15:36:53 +0100

    Add files via upload

---
+++ lesson_5_task_1/src/components/ListProducts/ProductItem.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container-product">
+        <img 
+        :src="productData.imgSrc" 
+        alt="raspberry" 
+        class="img"/>
+        <a 
+        :href="productData.link" 
+        class="link-title">
+        {{productData.title}}
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProductItem',
+        props: {
+            productData: {
+                type: Object,
+                default: () => ({}),
+            },
+        },
+    }
+</script>
+
+<style  scoped>
+.container-product{
+    padding: 10px;
+    text-align: center;
+    max-width: 200px;
+    height: 200px;
+    background-color: #fff;
+    border: solid 1px black;
+    border-radius: 10px;
+    transition: 0.8s;
+}
+
+.container-product:hover{
+    box-shadow: 10px 10px 10px grey;
+}
+
+.img{
+    width: 200px;
+    height: 170px;
+    border-radius: 10px;
+    border: solid 1px black;
+}
+
+.link-title{
+    text-decoration: none;
+    color: black;
+    font-size: 20px;
+    transition: 0.3s;
+}
+
+.link-title:hover{
+opacity: 0.5;
+}
+</style>
---
+++ lesson_5_task_1/src/data/products.js
@@ -0,0 +1,21 @@
+export const products = [
+    {
+        id: 1,
+        imgSrc: '/images/01.webp',
+        link: '#',
+        title: 'Малина'
+    },
+    {
+        id: 2,
+        imgSrc: '/images/02.jpg',
+        title: 'Малина свіжа',
+        link: '#',
+    },
+    {
+        id: 3,
+        imgSrc: '/images/03.jpg',
+        title: 'Малина свіжа лісова',
+        link: '#',
+    }
+
+]
